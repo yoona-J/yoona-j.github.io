23cd385f4fd1f898d0b6e41ddeda045eadb2b8dc
yoona_J <80026154+yoona-J@users.noreply.github.com>
2022-05-12 14:14:50 +0900

    Add files via upload

---
+++ precache-manifest.7e2cb0b5a487e0e289f0b068744a3900.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8893044a132f03daf774f4223ffa949d",
+    "url": "/artudent-exhibit-web/index.html"
+  },
+  {
+    "revision": "851b5a3d6614022d4fa3",
+    "url": "/artudent-exhibit-web/static/css/2.40e60438.chunk.css"
+  },
+  {
+    "revision": "0c58a75ca94dc8f21770",
+    "url": "/artudent-exhibit-web/static/css/main.e7fe21ae.chunk.css"
+  },
+  {
+    "revision": "851b5a3d6614022d4fa3",
+    "url": "/artudent-exhibit-web/static/js/2.c2de67ad.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "/artudent-exhibit-web/static/js/2.c2de67ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c58a75ca94dc8f21770",
+    "url": "/artudent-exhibit-web/static/js/main.9d936ba8.chunk.js"
+  },
+  {
+    "revision": "908261a83f80b4824838",
+    "url": "/artudent-exhibit-web/static/js/runtime-main.a8a9f3ee.js"
+  },
+  {
+    "revision": "5da5f9e7954e6c9b677024c3eac22e98",
+    "url": "/artudent-exhibit-web/static/media/AA.5da5f9e7.png"
+  },
+  {
+    "revision": "0e4ddf8e69d306025b5d33a8ef997569",
+    "url": "/artudent-exhibit-web/static/media/Background.0e4ddf8e.png"
+  },
+  {
+    "revision": "d06342e6c2a16884ea98e368eba3936a",
+    "url": "/artudent-exhibit-web/static/media/ExhibitionImg.d06342e6.jpg"
+  },
+  {
+    "revision": "0e4ddf8e69d306025b5d33a8ef997569",
+    "url": "/artudent-exhibit-web/static/media/artwork.0e4ddf8e.png"
+  },
+  {
+    "revision": "1f4b9743e77f3e90974b9ab55d545067",
+    "url": "/artudent-exhibit-web/static/media/exhibitImg.1f4b9743.jpg"
+  },
+  {
+    "revision": "6a0f40a2b069c6f7759886737b6b1674",
+    "url": "/artudent-exhibit-web/static/media/mainImg.6a0f40a2.png"
+  },
+  {
+    "revision": "c3f4ba9a0ecd5f6237a0cf07ab0bb131",
+    "url": "/artudent-exhibit-web/static/media/opacity.c3f4ba9a.png"
+  },
+  {
+    "revision": "9bced92ef5f15bf45d951b5ba5e0f810",
+    "url": "/artudent-exhibit-web/static/media/subscribeButton.9bced92e.png"
+  },
+  {
+    "revision": "8f329fa28d66e64350a1cec0e57e1bcb",
+    "url": "/artudent-exhibit-web/static/media/upload.8f329fa2.png"
+  }
+]);
