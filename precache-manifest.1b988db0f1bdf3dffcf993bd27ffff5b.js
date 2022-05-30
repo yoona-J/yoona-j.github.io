bdf9465c7ff1a669a0fb913ab0eccf0cd1df6b8e
yoona_J <80026154+yoona-J@users.noreply.github.com>
2022-05-30 11:34:09 +0900

    Add files via upload

---
+++ precache-manifest.1b988db0f1bdf3dffcf993bd27ffff5b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c5627d31b328a2380150bacaba946c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b01080443e96ad101a97",
+    "url": "/static/css/2.40e60438.chunk.css"
+  },
+  {
+    "revision": "488829af2e2071b8a1e7",
+    "url": "/static/css/main.9c85f77b.chunk.css"
+  },
+  {
+    "revision": "b01080443e96ad101a97",
+    "url": "/static/js/2.5613f07d.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "/static/js/2.5613f07d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "488829af2e2071b8a1e7",
+    "url": "/static/js/main.a9502250.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  },
+  {
+    "revision": "5da5f9e7954e6c9b677024c3eac22e98",
+    "url": "/static/media/AA.5da5f9e7.png"
+  },
+  {
+    "revision": "0e4ddf8e69d306025b5d33a8ef997569",
+    "url": "/static/media/Background.0e4ddf8e.png"
+  },
+  {
+    "revision": "d06342e6c2a16884ea98e368eba3936a",
+    "url": "/static/media/ExhibitionImg.d06342e6.jpg"
+  },
+  {
+    "revision": "3e4fd3e7cc269d4af227df2844094add",
+    "url": "/static/media/MyPage.3e4fd3e7.png"
+  },
+  {
+    "revision": "0e4ddf8e69d306025b5d33a8ef997569",
+    "url": "/static/media/artwork.0e4ddf8e.png"
+  },
+  {
+    "revision": "1f4b9743e77f3e90974b9ab55d545067",
+    "url": "/static/media/exhibitImg.1f4b9743.jpg"
+  },
+  {
+    "revision": "8f399185d30dcebe683efea5889d6d2b",
+    "url": "/static/media/library.8f399185.png"
+  },
+  {
+    "revision": "6a0f40a2b069c6f7759886737b6b1674",
+    "url": "/static/media/mainImg.6a0f40a2.png"
+  },
+  {
+    "revision": "9bced92ef5f15bf45d951b5ba5e0f810",
+    "url": "/static/media/subscribeButton.9bced92e.png"
+  },
+  {
+    "revision": "8f329fa28d66e64350a1cec0e57e1bcb",
+    "url": "/static/media/upload.8f329fa2.png"
+  }
+]);
