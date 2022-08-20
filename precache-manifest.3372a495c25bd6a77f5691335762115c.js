4efe1753a6fba3868460fcc0b47916fa7fd7e21a
yoona_J <yoonalove77@naver.com>
2022-08-20 23:32:38 +0900

    hoppy Project build

---
+++ precache-manifest.3372a495c25bd6a77f5691335762115c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a85f1e5be5cc95748ad487586af5ad70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "724b0dae4a2906d5e32a",
+    "url": "/static/css/2.051905ae.chunk.css"
+  },
+  {
+    "revision": "66a1a8d2e55bdc3119d5",
+    "url": "/static/css/main.fbd2a87f.chunk.css"
+  },
+  {
+    "revision": "724b0dae4a2906d5e32a",
+    "url": "/static/js/2.306bef0b.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.306bef0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66a1a8d2e55bdc3119d5",
+    "url": "/static/js/main.cb17dd54.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  },
+  {
+    "revision": "fd4a4c7727696d39c8c61d6caa9bdc18",
+    "url": "/static/media/art.fd4a4c77.png"
+  },
+  {
+    "revision": "bdd1a32d0ac781b4347c9b8877d5fc9b",
+    "url": "/static/media/character.bdd1a32d.png"
+  },
+  {
+    "revision": "b23b6668dc0b9c850f673a54f9aedbca",
+    "url": "/static/media/daily.b23b6668.png"
+  },
+  {
+    "revision": "9129f5a3da24768f68edd2d754f58e9d",
+    "url": "/static/media/exercise.9129f5a3.png"
+  },
+  {
+    "revision": "01cb625602c544bbe55f38d8bca1c24a",
+    "url": "/static/media/food.01cb6256.png"
+  },
+  {
+    "revision": "9d3140e38a3b67bf63c82e1bac77da0b",
+    "url": "/static/media/hobbyStory.9d3140e3.png"
+  },
+  {
+    "revision": "818f96fcb159ba893c6d8b30abbb65e3",
+    "url": "/static/media/logo.818f96fc.png"
+  },
+  {
+    "revision": "c5a0e87ad85c30fb7378086146f35920",
+    "url": "/static/media/music.c5a0e87a.png"
+  },
+  {
+    "revision": "5bc95b030e022aef145cd1267e220f57",
+    "url": "/static/media/trip.5bc95b03.png"
+  }
+]);
